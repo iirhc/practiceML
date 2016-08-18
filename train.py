defc45e94d4c07b49332c3f78b48f2cf5408c6aa
hc <hcchen@iir.csie.ncku.edu.tw>
2016-08-18 16:09:11 +0800

    train and predict

--- train.py
+++ train.py
@@ -3,29 +3,27 @@ import numpy as np
 import pickle
 import time
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
 from itertools import islice  # to ignore first line
 
-with open('feature_list.pkl', 'rb') as f:
-    feature_list = pickle.load(f)
-with open('classifier.pkl', 'rb') as f:
-    clf = pickle.load(f)
-print("load classifier finished")
-
-file2 = open("tweets_SEMEVAL_test2013.txt")
+file1 = open("tweets_SEMEVAL_train2013.txt")
 
 patten = "[\w'-]+"
 collect = []
+feature_set = set()
 
 i = 0
-for line in islice(file2, 1, None):
+for line in islice(file1, 1, None):
     match = re.findall(patten, line)
     match.pop(0)
     last = int(match.pop())
     aline = [match, last]
     collect.append(aline)
-print("load testing data finished")
+    for m in match:
+        feature_set.add(m)
+feature_list = list(feature_set)
+feature_list.sort()
+print("load training data finished")
+print(len(feature_list))
 
 feature = []
 gold_standard = []
@@ -45,9 +43,16 @@ feature = np.array(feature)
 gold_standard = np.array(gold_standard)
 print("numpy finished")
 
+clf = SVC(C=100)
 tstart = time.time()
-res = clf.predict(feature)
+clf.fit(feature, gold_standard)
 tend = time.time()
-print(accuracy_score(gold_standard, res))
-print(classification_report(gold_standard, res, [1, 0, -1]))
+print("SVC finished")
 print(tend - tstart)
+
+# save the classifier
+with open('feature_list.pkl', 'wb') as f:
+    pickle.dump(feature_list, f)
+with open('classifier.pkl', 'wb') as f:
+    pickle.dump(clf, f)
+print("save classifier finished")
